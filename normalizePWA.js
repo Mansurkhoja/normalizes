784e652ad4e5f8bfa0fc5e30f1c569f990d383a3
Mansur Temirkhojaev <mansur.atm@gmail.com>
2023-06-14 18:35:48 +0500

    Update normalizePWA.js added userAgent check if windows

--- normalizePWA.js
+++ normalizePWA.js
@@ -1,21 +1,27 @@
 export default () => {
+
   window.addEventListener('contextmenu',
     event => event.preventDefault()
   )
+
   window.addEventListener('wheel',
     event => {
       if (event.ctrlKey) event.preventDefault()
-    },
-    {
+    }, {
       'passive': false
     }
   )
+
   window.addEventListener('touchstart',
     event => {
       if (event.isTrusted && event.touches.length > 1) event.preventDefault()
-    },
-    {
+    }, {
       'passive': false
     }
   )
+
+  if (navigator.userAgent.search('Windows') !== -1) {
+    document.documentElement.classList.add('windows')
+  }
+
 }
